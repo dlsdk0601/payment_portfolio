0bf4aec27ddcf250c31a5645b2886e5c82a82259
dlsdk0601 <dlsdk0601@gmail.com>
2022-07-03 00:52:00 +0900

    publish heroku

--- app.js
+++ app.js
@@ -1,5 +1,22 @@
 import express from "express";
+import path from "path";
 
+// exporess
 const app = express();
 
-app.listen(8000, () => console.log("start"));
+const port = process.env.PORT || 5000;
+
+// 미들웨어 함수를 특정 경로에 등록
+app.use("/test", function (req, res) {
+  res.json({ greeting: "Hello World" });
+});
+
+app.listen(port);
+
+app.use(express.static(path.join(__dirname, "client/build")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname + "/client/build/index.html"));
+});
+
+console.log("start");
