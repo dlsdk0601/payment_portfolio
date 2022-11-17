238c1883ccf2a7b9b82d2b24640f03cbabf15243
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-18 00:37:39 +0900

    feat: 하드 코딩된 부분 추가

--- client/src/config/config.ts
+++ client/src/config/config.ts
@@ -6,8 +6,43 @@ export const apiEndPoint = {
 export const url = {
   localServer: "http://localhost:5000",
   localClient: "http://localhost:3000",
+  inicisJS: "https:///stdpay.inicis.com/stdjs/INIStdPay.js",
+  inicisMobileJs: "https://mobile.inicis.com/smart/payment/",
+};
+
+export const router = {
+  inicisFail: "/paymentfail",
+};
+
+export const inicisKey = {
+  mid: "INIpayTest",
+  mKey: "3a9503069192f207491d4b19bd743fc249a761ed94246c8c42fed06c3cd15a33",
 };
 
 export const env = {
   clientUrl: process.env.REACT_APP_BASEURL ?? url.localClient,
+  mobileReturnURL:
+    process.env.REACT_APP_RETURNURL_MOBILE ??
+    "http://localhost:5000/api/inicis/onetime-mobile",
+  mobileReturnWWWRUL:
+    process.env.REACT_APP_WWWRETURNURL_MOBILE ??
+    "https://www.localhost:5000/api/inicis/onetime-mobile",
+  returnURL:
+    process.env.REACT_APP_RETURNURL ??
+    "http://localhost:5000/api/inicis/onetime",
+  returnWWWURL:
+    process.env.REACT_APP_WWWRETURNURL ??
+    "https://www.localhost:5000/api/inicis/onetime",
+  closeURL:
+    process.env.REACT_APP_CLOSEURL ?? "http://localhost:5000/close-inicis",
+  closeWWWURL:
+    process.env.REACT_APP_WWWCLOSEURL ??
+    "http://www.localhost:5000/close-inicis",
+};
+
+export const alertText = {
+  noBuyername: "주문자를 입력하세요",
+  noBuyertel: "전화번호를 입력하세요",
+  noBuyeremail: "이메일를 입력하세요",
+  noPaymethod: "결제수단을 선택하세요",
 };
