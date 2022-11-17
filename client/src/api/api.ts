6b3c60ae56b60c6a7864dc2e4e56d6639254d53b
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-18 00:36:39 +0900

    refactor: API 타입 이름 및 async-await 수정

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,9 +1,11 @@
 import { apiEndPoint } from "../config/config";
 import Axios from "./Axios";
-import { IPaymentData } from "./Interface";
+import { paymentRegisterReq, paymentRegisterRes } from "./Interface";
 
 export const api = {
   // 결제 등록
-  paymentRegister: (body: IPaymentData) =>
-    Axios.post(apiEndPoint.inicisReady, body),
+  paymentRegister: async (
+    body: paymentRegisterReq
+  ): Promise<paymentRegisterRes> =>
+    await Axios.post(apiEndPoint.inicisReady, body),
 };
