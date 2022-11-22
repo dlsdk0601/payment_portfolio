5702496e37a6d3f5ff77b599430595d11584f492
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-23 01:32:31 +0900

    refactor: 결제 성공페이지에서 무통장입금의 경우 입금 정보 데이터 조건부 렌더링 처리

--- client/src/page/PaySuccessPage.tsx
+++ client/src/page/PaySuccessPage.tsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 import { ISelectOrder } from "../api/Interface";
 import Axios from "../api/Axios";
+import { paymentMethod } from "../config/config";
 
 export default function PaySuccessPage() {
   const { search } = useLocation();
@@ -11,6 +12,10 @@ export default function PaySuccessPage() {
   const [buyerName, setBuyerName] = useState("");
   const [goodName, setGoodName] = useState("");
   const [totPrice, setTotPrice] = useState("");
+  const [paymethod, setPaymethod] = useState("");
+  const [vactBankName, setVactBankName] = useState("");
+  const [vact_Date, setVact_Date] = useState("");
+  const [vact_Num, setVact_Num] = useState("");
 
   const onSelectOrderFetch = async () => {
     if (!oid) {
@@ -23,12 +28,25 @@ export default function PaySuccessPage() {
       return;
     }
 
-    const { tid, buyerName, goodName, totalPrice } = res.data;
+    const {
+      tid,
+      buyerName,
+      goodName,
+      totalPrice,
+      paymethod,
+      vactBankName,
+      VACT_Date,
+      VACT_Num,
+    } = res.data;
 
     setTid(tid);
     setBuyerName(buyerName);
     setGoodName(goodName);
     setTotPrice(totalPrice);
+    setPaymethod(paymethod);
+    setVactBankName(vactBankName ?? "");
+    setVact_Date(VACT_Date ?? "");
+    setVact_Num(VACT_Num ?? "");
   };
 
   useEffect(() => {
@@ -37,12 +55,25 @@ export default function PaySuccessPage() {
 
   return (
     <>
-      <div>성공</div>
-      <p>주문자: {buyerName}</p>
-      <p>구매상품: {goodName}</p>
-      <p>주문번호: {oid}</p>
-      <p>결제 금액: {totPrice}</p>
-      <p>tid(거래번호): {tid}</p>
+      <h1 className="title">성공</h1>
+      <p className="margin-bt">주문자: {buyerName}</p>
+      <p className="margin-bt">구매상품: {goodName}</p>
+      <p className="margin-bt">
+        주문번호: <br />
+        {oid}
+      </p>
+      <p className="margin-bt">결제 금액: {totPrice}</p>
+      <p className="margin-bt">
+        tid(거래번호): <br /> {tid}
+      </p>
+
+      {paymethod === paymentMethod.bank && (
+        <>
+          <p className="margin-bt">은행명: {vactBankName}</p>
+          <p className="margin-bt">입금 기한: {vact_Date}</p>
+          <p className="margin-bt">계좌번호: {vact_Num}</p>
+        </>
+      )}
     </>
   );
 }
