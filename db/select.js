b914fdaf8903a0038a90c38f85d932be162e0743
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-23 01:33:49 +0900

    feat: 에러 내용 알기위한 console 추가

--- db/select.js
+++ db/select.js
@@ -4,6 +4,7 @@ const selectDBHandle = async (query) => {
   return new Promise((resolve, reject) => {
     mariaDB.query(query, (err, rows) => {
       if (err) {
+        console.log(err);
         return resolve(false);
       }
       resolve(rows[0]);
