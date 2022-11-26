ea08ccf01c215e9b5de76322cde4b1422e32e80c
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-26 19:48:16 +0900

    refactor: kakopay API req, res interface 추가

--- client/src/api/Interface.tsx
+++ client/src/api/Interface.tsx
@@ -19,53 +19,18 @@ export interface ISelectResult extends IDefaultResponse {
   code: number;
 }
 
-export interface IpaymentResponse {
-  CARD_Quota: string;
-  CARD_ClEvent: string;
-  CARD_CorpFlag: string;
-  buyerTel: string;
-  parentEmail: string;
-  applDate: string;
-  buyerEmail: string;
-  p_Sub: string;
-  resultCode: string;
-  mid: string;
-  CARD_UsePoint: string;
-  CARD_Num: string;
-  authSignature: string;
-  ISP_CardCode: string;
-  tid: string;
-  EventCode: string;
-  goodName: string;
-  TotPrice: string;
-  payMethod: string;
-  CARD_MemberNum: string;
-  MOID: string;
-  CARD_Point: string;
-  currency: string;
-  CARD_PurchaseCode: string;
-  CARD_PrtcCode: string;
-  applTime: string;
-  goodsName: string;
-  CARD_CheckFlag: string;
-  FlgNotiSendChk: string;
-  CARD_Code: string;
-  CARD_BankCode: string;
-  CARD_TerminalNum: string;
-  ISP_RetrievalNum: string;
-  P_FN_NM: string;
+export interface IKakaoPaymentRequset {
   buyerName: string;
-  p_SubCnt: string;
-  applNum: string;
-  resultMsg: string;
-  CARD_Interest: string;
-  CARD_SrcCode: string;
-  CARD_ApplPrice: string;
-  CARD_GWCode: string;
-  custEmail: string;
-  CARD_PurchaseName: string;
-  CARD_PRTC_CODE: string;
-  payDevice: string;
+  item_name: string;
+  quantity: number;
+  total_amount: number;
+  tax_free_amount: number;
+  cid: string;
+  partner_order_id: string;
+  partner_user_id: string;
+  approval_url: string;
+  cancel_url: string;
+  fail_url: string;
 }
 
 export interface IKakaoReadyResponse extends IDefaultResponse {
@@ -99,7 +64,7 @@ export interface ISelectOrder {
   data: Idata;
 }
 
-export interface paymentRegisterReq {
+export interface IPaymentRegisterReq {
   buyername: string;
   buyertel: string;
   buyeremail: string;
@@ -111,7 +76,8 @@ export interface paymentRegisterReq {
   totalPrice: number;
 }
 
-export interface paymentRegisterRes {
+export interface IPaymentRegisterRes {
   result: boolean;
   msg: string;
 }
+
