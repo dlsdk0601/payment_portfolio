800dc001d653724d2f8757a2ddf02d8bcb55de8d
dlsdk0601 <dlsdk0601@gmail.com>
2022-11-26 19:46:11 +0900

    feat: kakopay 등록 API 추가

--- client/src/api/api.ts
+++ client/src/api/api.ts
@@ -1,18 +1,21 @@
 import { apiEndPoint } from "../config/config";
 import Axios from "./Axios";
 import {
+  IKakaoReadyResponse,
   ISelectOrder,
-  paymentRegisterReq,
-  paymentRegisterRes,
+  IPaymentRegisterReq,
+  IPaymentRegisterRes,
+  IKakaoPaymentRequset
 } from "./Interface";
 
 export const api = {
   // 결제 등록
   paymentRegister: async (
-    body: paymentRegisterReq
-  ): Promise<paymentRegisterRes> =>
+    body: IPaymentRegisterReq
+  ): Promise<IPaymentRegisterRes> =>
     await Axios.post(apiEndPoint.inicisReady, body),
 
   selectPayHistory: async (oid: string): Promise<ISelectOrder> =>
     await Axios.get(`${apiEndPoint.selectInicisData}${oid}`),
+  kakaopayRegister: async (body: IKakaoPaymentRequset): Promise<IKakaoReadyResponse> => await Axios.post(apiEndPoint.kakoPayReady, body)
 };
