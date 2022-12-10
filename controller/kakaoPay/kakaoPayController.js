044629406711a4f27faa56b522d02fcab8116609
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-10 23:31:21 +0900

    refactor: 하드코딩된 부분 모두 리펙토링

--- controller/kakaoPay/kakaoPayController.js
+++ controller/kakaoPay/kakaoPayController.js
@@ -3,6 +3,7 @@ import dotenv from "dotenv";
 import insertDBHandle from "../../db/insert.js";
 import selectDBHandle from "../../db/select.js";
 import updateDBHandle from "../../db/update.js";
+import { dbQuery, env, kakaoPayConst, responseMessage, url } from "../../config/config.js";
 
 dotenv.config();
 
@@ -20,30 +21,28 @@ async function kakaoPayReadyController(req, res) {
   } = body;
 
   if (quantity * 1000 !== total_amount) {
-    return res.json({ result: false, msg: "kakaoPay Ready API fail" });
+    return res.json({ result: false, msg: responseMessage.kakaoPayTotalPriceError });
   }
 
   const kakaoReady = await rp({
     method: "POST",
-    uri: "https://kapi.kakao.com/v1/payment/ready",
+    uri: url.kakaoReady,
     form: {
       ...body,
     },
     headers: {
-      Authorization: `KakaoAK ${process.env.NODE_KAKAO_ADMINKEY}`,
+      Authorization: `KakaoAK ${env.kakaoAdminKey}`,
       "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
     },
   });
 
   if (!kakaoReady) {
-    return res.json({ result: false, msg: "kakaoPay Ready API fail" });
+    return res.json({ result: false, msg: responseMessage.kakaoApiError });
   }
 
   const { tid, next_redirect_mobile_url, next_redirect_pc_url } =
     JSON.parse(kakaoReady);
 
-  const query =
-    "INSERT INTO kakaoPay (tid, oid, item_name, totalPrice, buyerName, partner_user_id) VALUES(?, ?, ? ,?, ?, ?)";
   const params = [
     tid,
     partner_order_id,
@@ -53,10 +52,10 @@ async function kakaoPayReadyController(req, res) {
     partner_user_id,
   ];
 
-  const isInsertDB = await insertDBHandle(query, params);
+  const isInsertDB = await insertDBHandle(dbQuery.insertKakaoPay, params);
 
   if (!isInsertDB) {
-    return res.json({ result: false, msg: "DataBase insert fail" });
+    return res.json({ result: false, msg: responseMessage.dbInsertFail });
   }
 
   return res.json({
@@ -74,28 +73,28 @@ async function kakaoPayApproveController(req, res) {
     body: { pg_token, oid },
   } = req;
 
-  const selectQuery = `SELECT tid, partner_user_id FROM kakaoPay where oid='${oid}'`;
+  const selectQuery = `${dbQuery.selectKakaoPay}'${oid}'`;
   const selectData = await selectDBHandle(selectQuery);
 
   if (!selectData) {
     return res.json({
       result: true,
-      msg: "approve fail",
+      msg: responseMessage.kakaoApproveError,
     });
   }
 
   const kakaoReady = await rp({
     method: "POST",
-    uri: "https://kapi.kakao.com/v1/payment/approve",
+    uri: url.kakaoApprove,
     form: {
-      cid: "TC0ONETIME",
+      cid: env.kakaoCid,
       tid: selectData.tid,
       partner_order_id: oid,
       partner_user_id: selectData.partner_user_id,
       pg_token,
     },
     headers: {
-      Authorization: `KakaoAK ${process.env.NODE_KAKAO_ADMINKEY}`,
+      Authorization: `KakaoAK ${env.kakaoAdminKey}`,
       "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
     },
   });
@@ -103,20 +102,19 @@ async function kakaoPayApproveController(req, res) {
   if (!kakaoReady) {
     return res.json({
       result: true,
-      msg: "approve fail",
-      kakaoPayApproveUrl: `/kakaopay-fail`,
+      msg: responseMessage.kakaoApproveError,
+      kakaoPayApproveUrl: url.kakaoFail,
     });
   }
 
-  const updateQuery = `UPDATE kakaoPay set isSuccess=? where tid=?`;
-  const params = [1, selectData.tid];
-  const updateDB = await updateDBHandle(updateQuery, params);
+  const params = [kakaoPayConst.paid, selectData.tid];
+  const updateDB = await updateDBHandle(dbQuery.updateKakaoPay, params);
 
   if (updateDB) {
     return res.json({
       result: true,
-      msg: "approve success",
-      kakaoPayApproveUrl: `/kakaopay-success/${oid}`,
+      msg: responseMessage.kakaoApprove,
+      kakaoPayApproveUrl: `${url.kakaoApproveClient}}${oid}`,
     });
   }
 }
@@ -127,29 +125,29 @@ async function kakaoPaySuccessController(req, res) {
     query: { tid },
   } = req;
 
-  const query = `SELECT oid, isSuccess FROM kakaoPay where tid='${tid}'`;
+  const query = `${dbQuery.selectKakaoPaySuccess}'${tid}'`;
   const selectData = await selectDBHandle(query);
 
   if (!selectData) {
     return res.json({
       result: true,
-      msg: "there is not Tid",
-      kakaoPayApproveUrl: `/kakaopay-fail`,
+      msg: responseMessage.kakaoTidError,
+      kakaoPayApproveUrl: url.kakaoFail,
     });
   }
 
-  if (selectData.isSuccess !== 1) {
+  if (selectData.isSuccess === kakaoPayConst.before) {
     return res.json({
       result: true,
-      msg: "pay fail",
-      kakaoPayApproveUrl: `/kakaopay-fail`,
+      msg: responseMessage.kakaoPayError,
+      kakaoPayApproveUrl: url.kakaoFail,
     });
   }
 
   return res.json({
     result: true,
     msg: null,
-    kakaoPayApproveUrl: `/kakaopay-success/${selectData.oid}`,
+    kakaoPayApproveUrl: `${url.kakaoSuccess}${selectData.oid}`,
   });
 }
 
@@ -159,29 +157,29 @@ async function kakaoPaySelectOrder(req, res) {
     query: { oid },
   } = req;
 
-  const query = `SELECT tid, oid, item_name, totalPrice, buyerName, isSuccess FROM kakaoPay where oid='${oid}'`;
+  const query = `${dbQuery.selectKakaoPayResult}'${oid}'`;
   const selectData = await selectDBHandle(query);
 
-  if (selectData) {
-    const { tid, oid, item_name, totalPrice, buyerName } = selectData;
-    return res.json({
-      result: true,
-      msg: null,
-      orderData: {
-        tid,
-        oid,
-        item_name,
-        totalPrice,
-        buyerName,
-      },
-    });
-  } else {
+  if (!selectData) {
     return res.json({
       result: false,
-      msg: "select fail",
+      msg: responseMessage.selectDataFail,
       orderData: null,
     });
   }
+  const { tid, item_name, totalPrice, buyerName } = selectData;
+
+  return res.json({
+    result: true,
+    msg: null,
+    orderData: {
+      tid,
+      oid,
+      item_name,
+      totalPrice,
+      buyerName,
+    },
+  });
 }
 
 export {
