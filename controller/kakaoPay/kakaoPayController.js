35537594c5b445a656265971c22b4d6d12c626a1
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-17 22:21:55 +0900

    refactor: 데이터 로직 서비스단으로 돌리고 간단하게 리펙토링

--- controller/kakaoPay/kakaoPayController.js
+++ controller/kakaoPay/kakaoPayController.js
@@ -1,70 +1,17 @@
-import rp from "request-promise";
-import dotenv from "dotenv";
-import insertDBHandle from "../../db/insert.js";
-import selectDBHandle from "../../db/select.js";
-import updateDBHandle from "../../db/update.js";
-import { dbQuery, env, kakaoPayConst, responseMessage, url } from "../../config/config.js";
-
-dotenv.config();
+import { responseMessage } from "../../config/config.js";
+import kakaoPayService from "../../service/kakaoPay/kakaoPayService.js";
 
 // 카카오페이 결제 준비
 async function kakaoPayReadyController(req, res) {
-  const { body } = req;
-
-  const {
-    quantity,
-    total_amount,
-    item_name,
-    partner_order_id,
-    buyerName,
-    partner_user_id,
-  } = body;
-
-  if (quantity * 1000 !== total_amount) {
+  try {
+    const data = await kakaoPayService.kakaoPayReadyService(req.body);
+    console.log("data");
+    console.log(data);
+    return res.json(data);
+  } catch (err) {
+    console.log(err);
     return res.json({ result: false, msg: responseMessage.kakaoPayTotalPriceError });
   }
-
-  const kakaoReady = await rp({
-    method: "POST",
-    uri: url.kakaoReady,
-    form: {
-      ...body,
-    },
-    headers: {
-      Authorization: `KakaoAK ${env.kakaoAdminKey}`,
-      "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  });
-
-  if (!kakaoReady) {
-    return res.json({ result: false, msg: responseMessage.kakaoApiError });
-  }
-
-  const { tid, next_redirect_mobile_url, next_redirect_pc_url } =
-    JSON.parse(kakaoReady);
-
-  const params = [
-    tid,
-    partner_order_id,
-    item_name,
-    total_amount,
-    buyerName,
-    partner_user_id,
-  ];
-
-  const isInsertDB = await insertDBHandle(dbQuery.insertKakaoPay, params);
-
-  if (!isInsertDB) {
-    return res.json({ result: false, msg: responseMessage.dbInsertFail });
-  }
-
-  return res.json({
-    result: true,
-    msg: null,
-    tid,
-    next_redirect_mobile_url,
-    next_redirect_pc_url,
-  });
 }
 
 // 카카오페이 결제 승인
@@ -73,48 +20,13 @@ async function kakaoPayApproveController(req, res) {
     body: { pg_token, oid },
   } = req;
 
-  const selectQuery = `${dbQuery.selectKakaoPay}'${oid}'`;
-  const selectData = await selectDBHandle(selectQuery);
-
-  if (!selectData) {
+  try {
+    const data = await kakaoPayService.kakaoPayApproveService(pg_token, oid);
+    return res.json(data);
+  } catch (err) {
+    console.log(err);
     return res.json({
-      result: true,
-      msg: responseMessage.kakaoApproveError,
-    });
-  }
-
-  const kakaoReady = await rp({
-    method: "POST",
-    uri: url.kakaoApprove,
-    form: {
-      cid: env.kakaoCid,
-      tid: selectData.tid,
-      partner_order_id: oid,
-      partner_user_id: selectData.partner_user_id,
-      pg_token,
-    },
-    headers: {
-      Authorization: `KakaoAK ${env.kakaoAdminKey}`,
-      "Content-type": "application/x-www-form-urlencoded;charset=utf-8",
-    },
-  });
-
-  if (!kakaoReady) {
-    return res.json({
-      result: true,
-      msg: responseMessage.kakaoApproveError,
-      kakaoPayApproveUrl: url.kakaoFail,
-    });
-  }
-
-  const params = [kakaoPayConst.paid, selectData.tid];
-  const updateDB = await updateDBHandle(dbQuery.updateKakaoPay, params);
-
-  if (updateDB) {
-    return res.json({
-      result: true,
-      msg: responseMessage.kakaoApprove,
-      kakaoPayApproveUrl: `${url.kakaoApproveClient}}${oid}`,
+      result: false, msg: responseMessage.kakaoPayError,
     });
   }
 }
@@ -125,30 +37,13 @@ async function kakaoPaySuccessController(req, res) {
     query: { tid },
   } = req;
 
-  const query = `${dbQuery.selectKakaoPaySuccess}'${tid}'`;
-  const selectData = await selectDBHandle(query);
-
-  if (!selectData) {
-    return res.json({
-      result: true,
-      msg: responseMessage.kakaoTidError,
-      kakaoPayApproveUrl: url.kakaoFail,
-    });
+  try {
+    const data = await kakaoPayService.kakaoPaySuccessService(tid);
+    return res.json(data);
+  } catch (err) {
+    console.log(err);
+    return res.json({ result: false, msg: responseMessage.selectDataFail });
   }
-
-  if (selectData.isSuccess === kakaoPayConst.before) {
-    return res.json({
-      result: true,
-      msg: responseMessage.kakaoPayError,
-      kakaoPayApproveUrl: url.kakaoFail,
-    });
-  }
-
-  return res.json({
-    result: true,
-    msg: null,
-    kakaoPayApproveUrl: `${url.kakaoSuccess}${selectData.oid}`,
-  });
 }
 
 // 결제 완료 페이지에서 조회
@@ -157,29 +52,13 @@ async function kakaoPaySelectOrder(req, res) {
     query: { oid },
   } = req;
 
-  const query = `${dbQuery.selectKakaoPayResult}'${oid}'`;
-  const selectData = await selectDBHandle(query);
-
-  if (!selectData) {
-    return res.json({
-      result: false,
-      msg: responseMessage.selectDataFail,
-      orderData: null,
-    });
+  try {
+    const data = await kakaoPayService.kakaoPaySelectOrderService(oid);
+    return res.json(data);
+  } catch (err) {
+    console.log(err);
+    return res.json({ result: false, msg: responseMessage.selectDataFail });
   }
-  const { tid, item_name, totalPrice, buyerName } = selectData;
-
-  return res.json({
-    result: true,
-    msg: null,
-    orderData: {
-      tid,
-      oid,
-      item_name,
-      totalPrice,
-      buyerName,
-    },
-  });
 }
 
 export {
