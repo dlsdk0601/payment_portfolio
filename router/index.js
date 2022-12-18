5b1f12773d628e9faeeb72735726469af987946e
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-18 21:47:20 +0900

    refactor: 정기결제 api router 등록

--- router/index.js
+++ router/index.js
@@ -1,12 +1,15 @@
 import express from "express";
 import inicisSimple from "./inicisSimple/index.js";
+import inicisSubscription from "./inicisSubscription/index.js";
 import kakaoPay from "./kakaopay/index.js";
+
 import test from "./test/index.js";
 
 const api = express.Router();
 
 api.use("/inicis", inicisSimple);
 api.use("/kakao", kakaoPay);
+api.use("/subscription", inicisSubscription);
 api.use(test);
 
 export default api;
