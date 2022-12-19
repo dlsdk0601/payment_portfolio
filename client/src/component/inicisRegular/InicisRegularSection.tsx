556839f65974a513c2188c919f8b3681468d7187
dlsdk0601 <dlsdk0601@gmail.com>
2022-12-19 16:42:55 +0900

    refactor: 상수 관련해서 모두 리펙토링

--- client/src/component/inicisRegular/InicisRegularSection.tsx
+++ client/src/component/inicisRegular/InicisRegularSection.tsx
@@ -6,7 +6,7 @@ import Axios from "../../api/Axios";
 import SHA256 from "../../utils/SHA256";
 import { onLoadScript, randomStringFunc } from "../../utils/utils";
 import {api} from "../../api/api";
-import {router, url} from "../../config/config";
+import {env, inicisKey, router, url} from "../../config/config";
 import {useNavigate} from "react-router-dom";
 
 const FORMTAG_ID = "SendPayForm_id";
@@ -186,15 +186,12 @@ export default function InicisRegularSection() {
           </div>
 
           {/* mid -> 실제 테스트 환경에서는 사용자 id, 테스트할 때는 INIBillTst 사용하면 된다. */}
-          <input type="hidden" name="mid" value={"INIBillTst"} />
+          <input type="hidden" name="mid" value={inicisKey.billingMid} />
 
           <input type="hidden" name="oid" value={oid} />
 
           {/* 인증 구분 D로 고정 */}
           <input type="hidden" name="authtype" value="D" />
-
-          {/* 제공기간 => 어느 단위로 결제를 할지 월단위 M2, 년단위 Y2  "YYYYMMDDYYYYMMDD":시작일종료일*/}
-          <input type="hidden" name="timestamp" value="M2" />
 
           {/* hash데이터 전문위조변조 mid+oid+timestamp+INILitekey를 해쉬화한 값 */}
           {/* 테스트 할때 INILitekey는 b09LVzhuTGZVaEY1WmJoQnZzdXpRdz09 넣으면 됨 */}
@@ -202,7 +199,7 @@ export default function InicisRegularSection() {
             type="hidden"
             name="hashdata"
             value={SHA256(
-              `INIBillTst${oid}${timeStamp}b09LVzhuTGZVaEY1WmJoQnZzdXpRdz09`
+              `${inicisKey.iniLiteKey}${oid}${timeStamp}${inicisKey.iniLiteKey}`
             )}
           />
 
@@ -212,14 +209,8 @@ export default function InicisRegularSection() {
             name="returnurl"
             value={
               window.location.href.includes("www")
-                ? `${
-                    process.env.REACT_APP_RETURNURL_MOBILE_REGULAR ||
-                    "http://localhost:5000/api/inicis/regular-mobile"
-                  }`
-                : `${
-                    process.env.REACT_APP_RETURNURL_MOBILE_REGULAR ||
-                    "https://www.localhost:5000/api/inicis/regular-mobile"
-                  }`
+                ? `${env.subscriptionMobileWWWReturnUrl}`
+                : `${env.subscriptionMobileReturnUrl}`
             }
           />
           {/* 결제창을 닫기 위해서 CloseInicis라는 페이지를 새로만드로 외부 js를 호출한다.   */}
@@ -294,15 +285,13 @@ export default function InicisRegularSection() {
           </div>
 
           {/* mid -> 실제 테스트 환경에서는 사용자 id, 테스트할 때는 INIBillTst 사용하면 된다. */}
-          <input type="hidden" name="mid" value={"INIBillTst"} />
+          <input type="hidden" name="mid" value={inicisKey.billingMid} />
 
           {/* 발급받은 mKey를 SHA256하면 된다. 테스트할 때는 3a9503069192f207491d4b19bd743fc249a761ed94246c8c42fed06c3cd15a33 를 사용하면 된다. */}
           <input
             type="hidden"
             name="mKey"
-            value={
-              "3a9503069192f207491d4b19bd743fc249a761ed94246c8c42fed06c3cd15a33"
-            }
+            value={inicisKey.mKey}
           />
           <input
             type="hidden"
@@ -321,7 +310,7 @@ export default function InicisRegularSection() {
           <input type="hidden" name="currency" value="WON" />
 
           {/* 정기결제를 위한 태그 */}
-          <input type="hidden" name="acceptmethod" value="BILLAUTH(Card)" />
+          <input type="hidden" name="acceptmethod" value={inicisKey.subscriptionBill} />
 
           {/* 결제 후 return하는 url로 결제 요청하는 도메인과 같아야 한다. 결제 성공 유무와 결제에 대한 데이터를 보낼 서버주소*/}
           <input
@@ -329,14 +318,8 @@ export default function InicisRegularSection() {
             name="returnUrl"
             value={
               window.location.href.includes("www")
-                ? `${
-                    process.env.REACT_APP_RETURNURL_MOBILE_REGULAR ||
-                    "http://localhost:5000/api/inicis/regular-mobile"
-                  }`
-                : `${
-                    process.env.REACT_APP_WWWRETURNURL_MOBILE_REGULAR ||
-                    "https://www.localhost:5000/api/inicis/regular-mobile"
-                  }`
+                ? `${env.subscriptionWWWReturnUrl}`
+                : `${env.subscriptionReturnUrl}`
             }
           />
           {/* 결제창을 닫기 위해서 CloseInicis라는 페이지를 새로만드로 외부 js를 호출한다.   */}
@@ -345,14 +328,8 @@ export default function InicisRegularSection() {
             name="closeUrl"
             value={
               window.location.href.includes("www")
-                ? `${
-                    process.env.REACT_APP_CLOSEURL_MOBILE_REGULAR ||
-                    "http://localhost:5000/close-inicis"
-                  }`
-                : `${
-                    process.env.REACT_APP_WWWCLOSEURL_MOBILE_REGULAR ||
-                    "http://www.localhost:5000/close-inicis"
-                  }`
+                ? `${env.closeWWWURL}`
+                : `${env.closeURL}`
             }
           />
           <button className="payment__button" type="submit">
